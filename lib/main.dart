6005ccb75821b872bd7275c727fb23452506cfbd
Can Utkan Karahan <ukarahan@sabanciuniv.edu>
2021-04-23 13:03:12 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cs310_app/routes/login.dart';
+import 'package:cs310_app/routes/signup.dart';
+import 'package:cs310_app/routes/walkthrough.dart';
+import 'package:cs310_app/routes/welcome.dart';
+import 'package:flutter/material.dart';
+
+bool opened = false;
+
+void walk(){
+  if (!opened){
+    WalkThrough();
+    opened = true;
+  }
+}
+
+void main() => runApp(MaterialApp(
+  //initialRoute: '/',
+  routes: {
+    '/': (context) => WalkThrough(),
+    '/welcome': (context) => Welcome(),
+    '/login': (context) => Login(),
+    '/signup': (context) => SignUp(),
+  },
+));
+
---
+++ lib/utils/styles.dart
@@ -0,0 +1,61 @@
+import 'package:cs310_app/utils/color.dart';
+import 'package:flutter/material.dart';
+
+
+
+final lightTextStyle = TextStyle(
+  color: AppColors.textColor,
+  fontSize: 14.0,
+  letterSpacing: 0.7,
+);
+
+final appBarStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 24.0,
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.7,
+);
+
+final mainTextStyle = TextStyle(
+  color: AppColors.textColor,
+  fontSize: 20.0,
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.7,
+);
+
+final secondaryTextStyle = TextStyle(
+  color:  AppColors.textColor,
+  fontSize: 18.0,
+  fontWeight: FontWeight.w500,
+  letterSpacing: -0.7,
+);
+
+final buttonStyle = OutlinedButton.styleFrom(
+  backgroundColor: AppColors.primary,
+);
+
+final buttonTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 20.0,
+  letterSpacing: -0.7,
+);
+
+final hintTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 15.0,
+  letterSpacing: 1.1,
+);
+
+
+final labelStyle = TextStyle(
+  color: AppColors.textColor,
+  fontSize: 13.0,
+);
+
+final errorStyle = TextStyle(
+  color: AppColors.errorColor,
+  fontSize: 12.0,
+  fontWeight: FontWeight.bold,
+);
+
+
